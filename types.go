fbb83d50137fd121d476d5962449c3b16973879c
Fnu Ferry <fferry@umd.edu>
2025-02-06 14:32:30 -0800

    add Ticket and Participant types

--- types.go
+++ types.go
@@ -50,6 +50,35 @@ type TicketType struct {
 	CreatedAt         time.Time `json:"created_at"`
 }
 
+type Ticket struct {
+	ID              int       `json:"id"`
+	TicketTypeID    int       `json:"ticket_type_id"`
+	OwnerId         int       `json:"owner_id"`
+	PurchasedAt     time.Time `json:"purchased_at"`
+	TicketTypeName  string    `json:"ticket_type_name"`
+	TicketTypePrice float32   `json:"ticket_type_price"`
+	EventID         int       `json:"event_id"`
+	EventName       string    `json:"event_name"`
+	EventStartAt    time.Time `json:"event_start_at"`
+	EventEndAt      time.Time `json:"event_end_at"`
+}
+
+// Participant represents a user who is attending an event. It contains
+// information of the user, the event, and the ticket they purchased to
+// attend the event.
+type Participant struct {
+	EventID         int       `json:"event_id"`
+	UserID          int       `json:"user_id"`
+	DisplayName     string    `json:"display_name"`
+	FullName        string    `json:"full_name"`
+	Gender          string    `json:"gender"`
+	TicketID        int       `json:"ticket_id"`
+	PurchasedAt     time.Time `json:"purchased_at"`
+	TicketTypeID    int       `json:"ticket_type_id"`
+	TicketTypeName  string    `json:"ticket_type_name"`
+	TicketTypePrice float32   `json:"ticket_type_price"`
+}
+
 func NewAccount(username string, password string, displayName string,
 	fullName string, gender byte, isHost bool) *Account {
 	return &Account{
